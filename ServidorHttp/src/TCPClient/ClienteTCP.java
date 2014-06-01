d2d864fda7370186c9394360cbe9a9426f352920
MatiasCS <matias.campos@alumnos.usm.cl>
2014-05-31 21:35:27 -0400

    Cambios menores
    Cambio en el delimitador utilizado para enviar el envio de mensajes del
    protocolo

--- ServidorHttp/src/TCPClient/ClienteTCP.java
+++ ServidorHttp/src/TCPClient/ClienteTCP.java
@@ -22,25 +22,29 @@ public class ClienteTCP {
     final static int puerto = 8082;
     InetAddress IP;
     String comandos[] = {"MEET","SENDMSG"}; //Arreglo con los comandos utilizados por el protocolo
-    DataOutputStream outServer;
     
     public ClienteTCP() throws UnknownHostException, IOException{
         this.IP = InetAddress.getByName("192.168.0.4"); 
         this.conexionCliente = new Socket("localhost", puerto);
-        this.outServer = new DataOutputStream(this.conexionCliente.getOutputStream());
     }
     
     public void MEET() throws IOException{
-        String mensajeTotal = comandos[0] + " Hola";
+        DataOutputStream outServer;
+        outServer = new DataOutputStream(this.conexionCliente.getOutputStream());
+        String mensajeTotal = comandos[0] + "##Hola";
         outServer.writeBytes(mensajeTotal + "\n");
         outServer.flush();
+        outServer.close();
     }
     
     //Metodo para enviar un mensaje
     public void SENDMSG(String mensaje, String IPDestino) throws IOException{
-        String mensajeTotal = comandos[0] + " " + IPDestino + " " + mensaje; //Variable que guarda el mensaje junto a los parametros adicionales necesarios
+        DataOutputStream outServer;
+        outServer = new DataOutputStream(this.conexionCliente.getOutputStream());
+        String mensajeTotal = comandos[0] + "##" + IPDestino + "##" + mensaje; //Variable que guarda el mensaje junto a los parametros adicionales necesarios
         outServer.writeBytes(mensajeTotal + "\n"); // Envio del mensajeTotal al servidor TCP
         outServer.flush();
+        outServer.close();
         
     }
 }
